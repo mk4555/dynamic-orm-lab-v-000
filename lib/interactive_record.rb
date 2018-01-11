719bf728d4614c404823ada6d5c6ffd4f45fb0f6
Min Kim <mk4555@nyu.edu>
2018-01-11 03:48:42 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,7 +55,7 @@ class InteractiveRecord
   end
 
   def self.find_by(attributes)
-    sql = ''
+    sql = nil
     attributes.each do |key, value|
       sql = "SELECT * FROM #{self.table_name} WHERE #{key}= '#{value}'"
     end
